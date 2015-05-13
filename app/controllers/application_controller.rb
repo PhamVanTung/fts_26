3bdb70f33a8d04b3cd2a7a0f45e8791de4f54298
Nguyen Ngoc Truong <nguyen.ngoc.truong@framgia.com>
2015-05-13 09:40:42 +0700

    edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-   protected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit :name, :avatar, :current_password
+    end
   end
 end
