7f3017bb4896679415462659d5d44507f834035c
Nguyen Ngoc Truong <nguyen.ngoc.truong@framgia.com>
2015-05-13 14:15:03 +0700

    exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,5 +1,24 @@
 class Exam < ActiveRecord::Base
+  enum status: [:wait, :testing, :done]
+
   belongs_to :user
   belongs_to :category
   has_many :results, dependent: :destroy
+
+  validates :category_id, presence: true
+
+  accepts_nested_attributes_for :results, allow_destroy: true
+
+  scope :order_time, ->{order created_at: :desc}
+
+  before_save {self.status = "wait"}
+  after_save :create_result
+
+  private
+  def create_result
+    @questions = Question.random_questions
+    @questions.each do |q|
+      Result.create(exam_id: self.id, question_id: q.id)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,38 @@
-User.create!(name:  "tung pham",
-             email: "tungadmin@gmail.com",
+User.create!(name:  "Nguyen Ngoc Truong",
+             email: "truongadmin@gmail.com",
              password:              "12345678",
              password_confirmation: "12345678",
              role: "admin")
+
+20.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,)
+end
+
+2.times do
+  name = Faker::Name.name
+  Category.create!(name: name)
+end
+
+categories = Category.order(:created_at).take(2)
+50.times do
+  content = Faker::Lorem.word
+  categories.each{|category| category.questions.create!(content: content)}
+end
+
+questions = Question.order(:created_at).take(100)
+3.times do
+  content = Faker::Lorem.word
+  questions.each{|question| question.answers.create!(content: content, correct: false)}
+end
+
+questions.each do |word|
+  content = Faker::Lorem.word
+  question.answers.create!(content: content, correct: true)
+end
+
